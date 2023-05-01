27c40f180e2ea346dca939c8555cdcaa28645684
danil <danil_12345677@mail.ru>
2023-05-01 19:58:35 +0700

    BackVercel13

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
 import bodyParser from "body-parser";
-import {sendBot} from "./Bot/bot.js";
 import product from './api/product.js'
+import axios from "axios";
 
 const app = express()
 const port = 3001
 const token = '1ea755ea1ea755ea1ea755ea4d1db49e8511ea71ea755ea7aecc9153d03fedd15bd59a8'
 const version = 5.131
 
+const TOKEN = '5993869459:AAGp-JUqJ8CwF9LQSiFskdq9AWV5y2rGa1E'
+const chat_id = '-1001935542586'
+const uri_api = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+
+
 const urlencodedPareser = bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://api.vk.com/', 'https://back-tvk.vercel.app/', 'http://localhost:3001', 'https://back-tvk.vercel.app/bot']
+    origin: ['http://localhost:3000', 'https://api.vk.com/', 'https://back-tvk.vercel.app/', 'http://localhost:3001']
 }))
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
@@ -37,7 +42,15 @@ app.get('/albom', async (req, res) => {
 
 app.post('/bot', urlencodedPareser, (req, res) => {
     try {
-        sendBot(req.body.name, req.body.value, req.body.selectOption)
+        let message = `<b>Заявка с сайта ТВК</b>\n>`
+        message += `<b>Имя того, кто отправил: </b> ${req.body.name}\n>`
+        message += `<b>Email: </b> ${req.body.value}\n>`
+        message += `<b>Процедура: </b> ${req.body.selectOption}`
+        axios.post(uri_api, {
+            chat_id: chat_id,
+            parse_mode: 'html',
+            text: message
+        })
         res.json({
             msg:'Отправка успешно'
         })
